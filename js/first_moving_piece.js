a747a2ef0e25424c4cdca485bf56be84c6d02725
ana108 <flutegurl108@hotmail.com>
2020-12-23 21:25:09 -0500

    decoration ideas

--- js/first_moving_piece.js
+++ js/first_moving_piece.js
@@ -1,16 +1,14 @@
-
-
 let centerX;
 let centerY;
-let mover;
-let mover2;
-let baseVector;
+
+let decorations = [];
 function Mover(m, x, y) {
     this.location = createVector(x, y);
     this.velocity = createVector(0, -2);
     this.acceleration = createVector(0, 0);
     this.topspeed = 10;
     this.mass = m;
+    this.colour = color(random(255), random(255), random(255));
 }
 Mover.prototype.update = function() {
     this.velocity.add(this.acceleration);
@@ -29,46 +27,46 @@ Mover.prototype.applyFroce = function(force) {
 }
 
 Mover.prototype.display = function() {
-    stroke(0);
-    fill(175);
-    ellipse(this.location.x, this.location.y, this.mass*16, this.mass*16);
+    stroke(255);
+    fill(this.colour);
+    rect(this.location.x, this.location.y, 8, 65);
 }
 Mover.prototype.checkEdges = function() {
-    if (this.location.y < 8) {
+    if (this.location.y < 0) {
         this.velocity.y *= -1;
-        baseVector.mult(-1);
-        this.location.y = 8;
-        let wind = createVector(0, 8);
+        this.location.y = 0;
+        let wind = createVector(0, 0);
         this.applyFroce(wind);
-    } else if (this.location.y >= 100) {
+    } else if (this.location.y >= 70) {
         this.velocity.y *= -1;
-        this.location.y = 100;
-        baseVector.mult(-1);
+        this.location.y = 70;
     }
 }
 
 function preload() {
 }
 function setup() {
-    centerX = windowWidth/2;
+    let cnv = createCanvas(windowWidth - 30, windowHeight - 30);
+    centerX = windowWidth-44/2;
     centerY = windowHeight/2;
-    let cnv = createCanvas(windowWidth - 40, windowHeight - 40);
     cnv.mousePressed(canvasPressed);
-    mover = new Mover(1, centerX, centerY);
-    mover2 = new Mover(1, centerX+25, centerY);
-    baseVector = createVector(0, -1);
+    let start = 4;
+    let total = Math.round(windowWidth/25);
+    for(let i = 0; i < total; i++) {
+        decorations.push(new Mover(1, start, i%5));
+        start = start+25;
+    }
 }
 
 function draw() {
     background(255);
     strokeWeight(2);
-    mover.update();
-    mover.checkEdges();
-    mover.display();
-
-    mover2.update();
-    mover2.checkEdges();
-    mover2.display();
+    decorations.forEach(mover => {
+        mover.display();
+        mover.update();
+        mover.checkEdges();
+    });
+   
 }
 
 function canvasPressed() {
